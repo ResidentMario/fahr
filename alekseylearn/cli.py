d359f1929d4bad70a37bdc724f7f5cb8c96ad0bd
ResidentMario <aleksey.bilogur@gmail.com>
2019-01-27 22:38:24 -0500

    Seperate run into fit and fetch.

--- alekseylearn/cli.py
+++ alekseylearn/cli.py
@@ -1,5 +1,6 @@
 import click
 from .alekseylearn import TrainJob
+from .alekseylearn import fetch as _fetch
 
 @click.group()
 def cli():
@@ -7,23 +8,37 @@ def cli():
 
 
 @click.command(
-    name='run', context_settings=dict(ignore_unknown_options=True, allow_extra_args=True)
+    name='fit', context_settings=dict(ignore_unknown_options=True, allow_extra_args=True)
 )
 @click.argument('training_artifact_path')
-@click.argument('model_artifact_path')
 @click.option('--driver', default='sagemaker', help='Driver to be used for running the train job.')
 @click.option('--envfile', default=None, help='Code environment definition file to build with.')
 @click.option('--overwrite', default=False, help='If true, overwrite existing training artifacts.')
 @click.pass_context
-def run(ctx, training_artifact_path, model_artifact_path, driver, envfile, overwrite):
+def fit(ctx, training_artifact_path, driver, envfile, overwrite):
+    config = fmt_ctx_args(ctx.args)
+    j = TrainJob(training_artifact_path, driver=driver, overwrite=overwrite, config=config)
+    j.fit()
+
+
+@click.command(name='fetch')
+@click.argument('local_path')
+@click.argument('tag')
+@click.argument('remote_path')
+@click.option('--driver', default='sagemaker', help='Driver to be used for running the train job.')
+@click.option('--extract', default='sagemaker', help='Driver to be used for running the train job.')
+def fetch(local_path, tag, remote_path, driver, extract):
+    _fetch(local_path, tag, remote_path, driver=driver, extract=extract)
+
+
+def fmt_ctx_args(ctx):
     def fmt_ctx_arg(arg):
         if arg[:9] != '--config.':
             raise ValueError("Configuration arguments must be prefixed with `config`")
         return arg[9:].split('=')
 
-    config = dict([fmt_ctx_arg(arg) for arg in ctx.args])
-    j = TrainJob(training_artifact_path, driver=driver, overwrite=overwrite, config=config)
-    j.run(model_artifact_path)
+    return dict([fmt_ctx_arg(arg) for arg in ctx.args])
 
 
-cli.add_command(run)
+cli.add_command(fit)
+cli.add_command(fetch)
